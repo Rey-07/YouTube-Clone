2671e11b36554287f22fcbcd3feaeb806a56bded
Rey-07 <tripathirey07@gmail.com>
2024-04-10 02:25:19 +0530

    Video card started

--- my-app/src/components/VideoCard.tsx
+++ my-app/src/components/VideoCard.tsx
@@ -2,11 +2,18 @@ export function VideoCard() {
   return (
     <div>
       <img src="/photo.jpg"></img>
-      <div className="grid grid-col-12">
+      <div className="grid grid-cols-12 pb-2">
         <div className="col-span-1">
           <img className="rounded-full w-20 h-20" src="/photo.jpg"></img>
         </div>
-        <div className="col-span-11">Jawan: Chaleya [Hindi] Shahrukh Khan</div>
+
+        <div className="col-span-11 pl-5">
+          <div>Jawan: Chaleya [Hindi] | Shahrukh Khan</div>
+          <div className="col-span-11 pl-0 text-gray-400">Harkirat Singh</div>
+          <div className="col-span-11 pl-0 text-gray-400">
+            48M+ | 13 Days Ago
+          </div>
+        </div>
       </div>
     </div>
   );
