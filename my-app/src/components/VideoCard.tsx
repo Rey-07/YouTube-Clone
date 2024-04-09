c68b2b34b5a963d7621a740185c4277f92478716
Rey-07 <tripathirey07@gmail.com>
2024-04-10 02:15:40 +0530

    Initial commit

---
+++ my-app/src/components/VideoCard.tsx
@@ -0,0 +1,13 @@
+export function VideoCard() {
+  return (
+    <div>
+      <img src="/photo.jpg"></img>
+      <div className="grid grid-col-12">
+        <div className="col-span-1">
+          <img className="rounded-full w-20 h-20" src="/photo.jpg"></img>
+        </div>
+        <div className="col-span-11">Jawan: Chaleya [Hindi] Shahrukh Khan</div>
+      </div>
+    </div>
+  );
+}
